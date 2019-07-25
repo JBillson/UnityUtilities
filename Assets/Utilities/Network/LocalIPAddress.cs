e4c9fbbc0ca108efa102dde28e62ec6e47391864
Justin Billson <justin.billson@gmail.com>
2019-07-25 15:34:25 +0200

    Added GetLocalIPAddress.cs

---
+++ Assets/Utilities/Network/LocalIPAddress.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace _RepController.VideoSystem
+{
+    public abstract class LocalIPAddress
+    {
+        public static string GetLocalIP()
+        {
+            var localIP = "0.0.0.0";
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily != AddressFamily.InterNetwork) continue;
+
+                localIP = ip.ToString();
+                break;
+            }
+
+            return localIP;
+        }
+    }
+}

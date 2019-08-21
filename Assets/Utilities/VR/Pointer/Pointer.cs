ea3d415befa345b5086b3b19ec16d0a41de0f940
Justin Billson <justin.billson@gmail.com>
2019-08-21 14:04:44 +0200

    Readded the VR Utilities

---
+++ Assets/Utilities/VR/Pointer/Pointer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityUtilities.Assets.Utilities.VR.Pointer
+{
+    public class Pointer : MonoBehaviour
+    {
+        private Selectable _selectable;
+
+        //TODO: Change keycode to UnityXRInput
+        [SerializeField] private KeyCode select;
+
+        private void Update()
+        {
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit,
+                Mathf.Infinity))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance,
+                    Color.white);
+
+                if (hit.transform.GetComponent<Selectable>())
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance,
+                        Color.yellow);
+
+                    _selectable = hit.transform.GetComponent<Selectable>();
+
+                    if (_selectable == null) return;
+
+                    _selectable.StartHovering();
+
+                    if (Input.GetKeyDown(select))
+                        _selectable.Selected();
+
+                    var amount = Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger");
+                    print(amount);
+                    if (amount > 0.7f)
+                    {
+                        _selectable.Selected();
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        _selectable.StopHovering();
+                        _selectable = null;
+                    }
+                    catch (Exception)
+                    {
+                        _selectable = null;
+                    }
+                }
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                try
+                {
+                    _selectable.StopHovering();
+                    _selectable = null;
+                }
+                catch (Exception)
+                {
+                    _selectable = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Utilities/VR/Pointer/Selectable.cs
@@ -0,0 +1,49 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityUtilities.Assets.Utilities.VR.Pointer
+{
+    public class Selectable : MonoBehaviour
+    {
+        [Header("Settings")] public bool canReset;
+        [ShowIf("canReset")] public float resetTime = 2f;
+
+        [Header("Events")] public UnityEvent onHoverStarted;
+        public UnityEvent onHoverStopped;
+        public UnityEvent onSelected;
+        public UnityEvent onSelectableReset;
+
+        private bool _isHovering;
+        private bool _hasBeenSelected;
+
+        public void StartHovering()
+        {
+            if (_isHovering) return;
+            _isHovering = true;
+            onHoverStarted?.Invoke();
+        }
+
+        public void Selected()
+        {
+            if (_hasBeenSelected) return;
+
+            _isHovering = false;
+            _hasBeenSelected = true;
+            onSelected?.Invoke();
+            StartCoroutine(Delays.DelayedAction(ResetSelectable, resetTime));
+        }
+
+        public void StopHovering()
+        {
+            _isHovering = false;
+            onHoverStopped?.Invoke();
+        }
+
+        public void ResetSelectable()
+        {
+            _hasBeenSelected = false;
+            onSelectableReset?.Invoke();
+        }
+    }
+}
